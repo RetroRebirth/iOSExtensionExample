d85d9aa69ca5320cd7afd6251414f6dd382e62ce
Christopher Williams <chrsw49@gmail.com>
2015-10-22 15:18:53 -0700

    basic share extension working. now get URL

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # iOSExtensionExample
-A container app that showcases multiple extension functionalities from tutorials or myself.
-
-# Tutorials
-MeterFeet: http://swiftiostutorials.com/tutorial-creating-ios-app-extension-ios-8-perform-custom-actions-safari-content/
-ImgurShare: http://www.appcoda.com/ios8-share-extension-swift/
+Messing around with iOS extensions.
---
+++ URLShareExtensionExample/URLShareExtensionExample/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  URLShareExtensionExample
+//
+//  Created by Christopher Williams on 10/22/15.
+//  Copyright © 2015 Christopher Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let sharedDefaults = NSUserDefaults(suiteName: "group.me.christopherwilliams.iOSExtensionExample")
+        
+        self.label.text = sharedDefaults?.objectForKey("stringKey") as? String
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
